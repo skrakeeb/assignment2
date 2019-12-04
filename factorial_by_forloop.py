950b2c2c0319fa8690338ce78e4a79cea0a22947
skrakeeb <57462022+skrakeeb@users.noreply.github.com>
2019-12-04 17:49:12 +0530

    Update factorial_by_forloop.py

--- factorial_by_forloop.py
+++ factorial_by_forloop.py
@@ -1,9 +1,11 @@
 # calculating factorial by for loop:
 
 from math import*
+import timeit
+
 s=1
 n=int(input("Give a positive integer:"))
 
 for i in range(n):
    s=s*(i+1)
-print ("The value of factorial ",n," is ",s)
+print ("The value of factorial ",n," is ",s,"\nTime taken to run the code: ",timeit.timeit)
