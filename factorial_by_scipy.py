5b67cb4c4fd1b8aa3dd4cda4a15e0be72b599376
skrakeeb <57462022+skrakeeb@users.noreply.github.com>
2019-12-04 17:47:03 +0530

    Update factorial_by_scipy.py

--- factorial_by_scipy.py
+++ factorial_by_scipy.py
@@ -1,7 +1,8 @@
 #factorial using scipy library:
 
 import scipy.special as sp
+import timeit
 
 n=int(input("Give a positive integer: "))
 s=sp.factorial(n)
-print ("The value of factorial ",n," is ",s)
+print ("The value of factorial ",n," is ",s,"\nTime taken to run the code: ",timeit.timeit)
