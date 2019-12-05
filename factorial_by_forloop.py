daf3ee6cc1da8719a0101f7bc6ab9e2eba0916d7
skrakeeb <57462022+skrakeeb@users.noreply.github.com>
2019-12-05 15:01:24 +0530

    Update factorial_by_forloop.py

--- factorial_by_forloop.py
+++ factorial_by_forloop.py
@@ -8,4 +8,4 @@ n=int(input("Give a positive integer:"))
 
 for i in range(n):
    s=s*(i+1)
-print ("The value of factorial ",n," is ",s,"\nTime taken to run the code: ",timeit.timeit)
+print ("The value of factorial ",n," is ",s,"\nTime taken to run the code: ",timeit.timeit())
